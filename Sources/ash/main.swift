466da42a39c24e39890da4811db68255c21bca52
David Boeke <david@boeke.com>
2026-06-13 14:28:16 -0400

    Release v0.1.1

--- Sources/ash/main.swift
+++ Sources/ash/main.swift
@@ -17,7 +17,7 @@ enum Style {
 
 func err(_ s: String) { FileHandle.standardError.write(Data((s + "\n").utf8)) }
 
-let version = "0.1.0"
+let version = "0.1.1"
 
 func printUsage() {
     print("""
